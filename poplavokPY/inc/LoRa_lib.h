9177ee36eaf37dd8a0026eddbbafb3c8c97b6e08
ovostch42 <mattveyxes@mail.ru>
2026-06-12 16:39:17 +0300

    updated h

--- poplavokPY/inc/LoRa_lib.h
+++ poplavokPY/inc/LoRa_lib.h
@@ -4,3 +4,4 @@
 /* Exported function prototypes -----------------------------------------------*/
 void LORALIB_LORA_Init();
 void LORALIB_LORA_SendPacket(uint8_t *data, uint8_t len);
+uint8_t LORALIB_LORA_ReceivePacket(uint8_t *buffer);
